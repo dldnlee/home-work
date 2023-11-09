9c02cbf0c9ffe283019160b1ad4b102147941a20
dldnlee <sungeun8877@gmail.com>
2023-11-10 01:41:42 +0900

    functioning input field event listener

--- mission-02/mission-02.js
+++ mission-02/mission-02.js
@@ -1,2 +1,24 @@
-const emailField = document.querySelector(".email-input");
-const pwField = document.querySelector(".pw-input");
+const inputField = document.getElementById("login-email");
+const errorMsg = document.getElementById('email-error');
+
+// function validCheck(){
+//   if(field.checkValidity() === true) {
+//     errorMsg.innerHTML = "right inputt";
+//   } else {
+//     errorMsg.innerHTML = "wrong inuput";
+//   }
+// };
+
+inputField.addEventListener('keydown', function(e) {
+  if(inputField.checkValidity() === true) {
+    errorMsg.innerHTML = "right inputt";
+  } else {
+    errorMsg.innerHTML = "wrong inuput";
+  }
+
+  inputField.addEventListener('blur', function (e) {
+    if(inputField.value === "") {
+      errorMsg
+    }
+  })
+})
