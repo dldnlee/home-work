801f39010e6d60dc2bd86356bb8200fdda32aee5
dldnlee <sungeun8877@gmail.com>
2023-11-10 21:02:32 +0900

    validator toggle on empty field

--- mission-02/mission-02.js
+++ mission-02/mission-02.js
@@ -2,46 +2,16 @@ const emailField = document.getElementById("login-email");
 const emailIcon = document.querySelector('.email-validator-mark');
 const emailLabel = document.querySelector('.input-label-email')
 
-const pwField = document.getElementById("login-password");
-const pwIcon = document.querySelector('.pw-validator-mark');
-const pwLabel = document.querySelector('.input-label-password')
-
-// function eventListeners(input, icon, label) {
-//   input.addEventListener('keydown', function(e) {
-//     if(input.checkValidity() === true) {
-//       icon.classList.remove('hidden');
-//       icon.src = "./images/valid-icon.svg";
-//     } else {
-//       icon.classList.remove('hidden');
-//       icon.src = "./images/invalid-icon.svg";
-//     }
-//   })
-  
-//   input.addEventListener('blur', function(e) {
-//     if(input.value === "") {
-//       icon.classList.add('hidden');
-//       label.classList.remove('--is-active');
-//     } else {
-//       icon.classList.remove('hidden');
-//     }
-//   })
-
-//   input.addEventListener('focus', function(e) {
-//     label.classList.add('--is-active');
-//   })
-// }
-
-
-// eventListeners(emailField, emailIcon, emailLabel)
-// eventListeners(pwField, pwIcon, pwLabel)
-
 
 function email() {
-  emailField.addEventListener('keydown', function(e) {
+  emailField.addEventListener('keyup', function(e) {
     if(emailField.checkValidity() === true) {
       emailIcon.classList.remove('hidden');
       emailIcon.src = "./images/valid-icon.svg";
-    } else {
+    } else if (emailField.value === "") {
+      emailIcon.classList.add('hidden');
+    } 
+    else {
       emailIcon.classList.remove('hidden');
       emailIcon.src = "./images/invalid-icon.svg";
     }
@@ -63,15 +33,22 @@ function email() {
 
 email();
 
+// password field DOM elements
+
+const pwField = document.getElementById("login-password");
+const pwIcon = document.querySelector('.pw-validator-mark');
+const pwLabel = document.querySelector('.input-label-password')
 const pwVisible = document.querySelector('.pw-visibility');
 const pwEye = document.querySelector('.eye');
 
 function password() {
-  pwField.addEventListener('keydown', function(e) {
-    if(pwField.checkValidity() === true) {
+  pwField.addEventListener('keyup', function(e) {
+    if(pwField.checkValidity() === true && pwField.value !== "") {
       pwIcon.classList.remove('hidden');
       pwIcon.src = "./images/valid-icon.svg";
       pwVisible.classList.remove('hidden');
+    } else if(pwField.value === "") {
+      pwIcon.classList.add('hidden');
     } else {
       pwIcon.classList.remove('hidden');
       pwIcon.src = "./images/invalid-icon.svg";
@@ -90,13 +67,11 @@ function password() {
 
   pwField.addEventListener('focus', function(e) {
     pwLabel.classList.add('--is-active');
-
   })
 
   pwVisible.addEventListener('click', function(e) {
     const type = pwField.getAttribute('type') === "password" ? "text" : "password";
     pwField.setAttribute("type", type);
-
 
     pwEye.classList.toggle("eye");
   })
